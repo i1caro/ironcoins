0887e036696455949cd9baeaba5c0b7221d2c7d8
i1caro <rnunojanela@gmail.com>
2014-01-17 00:05:01 +0000

    Test for view

--- www/main.py
+++ www/main.py
@@ -1,25 +1,19 @@
-from flask import Flask, request
-from flask.ext.restful import Resource, Api
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask.ext.httpauth import HTTPBasicAuth
 
+import json
+
+auth = HTTPBasicAuth()
 app = Flask(__name__)
-api = Api(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-todos = {}
-class MapView(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(MapView, '/<string:todo_id>')
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def map_view():
+    with open('./ironcoins/www/tests/map_view.json') as data_file:
+        data = json.load(data_file)
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
